7163ba9fa98ab4c04c3bc4a07c35b3b43808e360
edi2xr <mutuaadrian73@gmail.com>
2025-08-13 22:57:35 +0300

    add dragon stats

--- battlegame.py
+++ battlegame.py
@@ -7,3 +7,12 @@ human = "Human"
 wizard_hp = 70
 elf_hp = 100
 human_hp = 150
+
+# Damage characters
+wizard_damage = 150
+elf_damage = 100
+human_damage = 20
+
+# Add dragon stats
+dragon_hp = 300
+dragon_damage = 50
