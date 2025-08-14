6590fc278c8e558955fa7bd531c61ca09f6cd6ec
Nijhan <najmashaaban6@gmail.com>
2025-08-14 16:13:59 +0300

    fixed a bug

--- battlegame.py
+++ battlegame.py
@@ -74,30 +74,26 @@ print("Damage:", my_damage)
 
 
 
-#task4
 
-# Create copies of HP so originals stay unchanged
+# Task 4: Battle Simulation
 current_dragon_hp = dragon_hp
 current_my_hp = my_hp
+
 while True:
-    #player attacks the dragon
-    current_dragon_hp = my_damage
+    # Player attacks the dragon
+    current_dragon_hp -= my_damage
     print(f"The {character} attacks and deals {my_damage} damage to the Dragon.")
     print(f"The Dragon's HP is now {current_dragon_hp}.")
 
-    #check if dragon is defeated
     if current_dragon_hp <= 0:
-        print("The Dragon has lost the battle! You win!\n")
+        print("The Dragon has lost the battle! You win!")
         break
-    #dragon attacks the player
-    current_my_hp = dragon_damage
+
+    # Dragon attacks the player
+    current_my_hp -= dragon_damage
     print(f"The Dragon attacks and deals {dragon_damage} damage to the {character}.")
     print(f"The {character}'s HP is now {current_my_hp}.")
 
-
-    #check if player is defeated
     if current_my_hp <= 0:
-        print(f"The {character} has lost the battle! You lose!\n")
+        print(f"The {character} has lost the battle! You lose!")
         break
-
-
