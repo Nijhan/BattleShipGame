383e39bdd6f34f388802b9c339eb0a73360874fc
Nijhan <najmashaaban6@gmail.com>
2025-08-14 16:34:35 +0300

    fixed a bug

--- battlegame.py
+++ battlegame.py
@@ -1,10 +1,10 @@
 
 
 
+
 #Task 2: Prompt Player Choice
 character_choice=input("choose your character")
 print("1)Wizard")
 print("2) Elf")
 print("3) Human")
 print("You chose option:", character_choice)
-
